99d012d4881f10586a60dc5b158c8b90e6669fee
yujieto <medmind2014@gmail.com>
2017-06-05 17:03:44 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 'use strict';
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
+
 function fib(n) {
-    if (n === 0) {
-        return 0;
-    } else if(n ===1) {
-        return 1;
+    if (memo.has(n)) {
+        return memo.get(n);
     }
 
-    return fib(n - 1) + fib(n - 2);//再帰関数
+    const value = fib(n - 1) + fib(n - 2);//再帰関数
+    
+    memo.set(n, value);//memoに計算結果を保持
+
+    return value;
 }
 
-const length = 40;
+const length = 1000;
 for (let i =0; i <= length; i++) {
     console.log(fib(i));
 }
